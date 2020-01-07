060a4bd0e8caa9db757080fef09370ae3bfb6303
MeloMurd <opponib3@gmail.com>
2020-01-07 13:07:02 +0200

    Delete Some braces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,19 +17,20 @@ public class Main {
         put("Зигангеров", "Сергей");
     }};
 
+
+
     public static void main(String[] args) {
         HashMap<String, String> hmCopy = new HashMap<>();
         List<String> keysForDelete = new ArrayList<>();
 
-        for (HashMap.Entry<String, String> hm : HM.entrySet()) {
+        for (HashMap.Entry<String, String> hm : HM.entrySet())
             if (hmCopy.put( hm.getValue(), hm.getKey()) != null) // перевіряємо, чи була заміна старого аналогічного ключа (ім'я) на такеж саме
                keysForDelete.add(hm.getValue()); // додаємо позиції для видалення
-        }
 
         //ВСЕ, наразі маємо hmCopy без жодного повторення ім'я, при цьому маємо перелік імен для видалення
-        for (String keyForDelete:keysForDelete) {
+        for (String keyForDelete:keysForDelete)
              hmCopy.remove(keyForDelete);
-        }
+
 
         System.out.println("Unique names List");
         for (HashMap.Entry<String, String> hm : hmCopy.entrySet()) {
